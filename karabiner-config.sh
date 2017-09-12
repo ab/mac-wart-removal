c1215eb7cab9a65b318495fcd444fe9f237e3f7b
Andy Brody <andy.brody@beta.dhs.gov>
2017-09-12 02:17:33 -0400

    Update Karabiner to work with Sierra.

--- karabiner-config.sh
+++ karabiner-config.sh
@@ -1,25 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-# Keyboard settings for non-mac external keyboards
+# original Karabiner, doesn't work on Sierra
+karabiner_classic_install() {
+    # Keyboard settings for non-mac external keyboards
 
-# This script sets karabiner configuration to remap the alt and win keys for
-# more usable setup for non-mac external keyboards. It was generated with
-# `karabiner export`.
+    # This script sets karabiner configuration to remap the alt and win keys for
+    # more usable setup for non-mac external keyboards. It was generated with
+    # `karabiner export`.
 
-cli=/usr/local/bin/karabiner
+    cli=/usr/local/bin/karabiner
 
-$cli set general.dont_remap_apple_keyboard 1
-/bin/echo -n .
-$cli set general.dont_remap_apple_pointing 1
-/bin/echo -n .
-$cli set remap.commandL2optionL 1
-/bin/echo -n .
-$cli set remap.optionL2commandL 1
-/bin/echo -n .
-$cli set remap.optionrcommandr 1
-/bin/echo -n .
-$cli set repeat.initial_wait 583
-/bin/echo -n .
-$cli set repeat.wait 33
-/bin/echo -n .
-/bin/echo
+    $cli set general.dont_remap_apple_keyboard 1
+    /bin/echo -n .
+    $cli set general.dont_remap_apple_pointing 1
+    /bin/echo -n .
+    $cli set remap.commandL2optionL 1
+    /bin/echo -n .
+    $cli set remap.optionL2commandL 1
+    /bin/echo -n .
+    $cli set remap.optionrcommandr 1
+    /bin/echo -n .
+    $cli set repeat.initial_wait 583
+    /bin/echo -n .
+    $cli set repeat.wait 33
+    /bin/echo -n .
+    /bin/echo
+}
+
+# Karabiner Elements, works on Sierra
+karabiner_elements_install() {
+    set -x
+    mkdir -vp ~/.config/karabiner
+    cp -iv "$(dirname "$0")/karabiner.json" ~/.config/karabiner/
+}
+
+karabiner_elements_install
